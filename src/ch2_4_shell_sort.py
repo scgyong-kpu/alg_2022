965679cdfae8c0b2f9a6a58b4d3d50b3dc920c87
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-09-07 01:19:31 +0900

    Visualizer 를 사용하는 버전으로 원상복귀. gap sequence 를 Hibbard + Ciura 로 설정.

--- src/ch2_4_shell_sort.py
+++ src/ch2_4_shell_sort.py
@@ -1,7 +1,7 @@
-# from data_unsorted import numbers
-from data_unsorted_a_lot import numbers
+from data_unsorted import numbers
+# from data_unsorted_a_lot import numbers
 from random import randint, seed, shuffle
-# from vis import ShellSortVisualizer as Visualizer
+from vis import ShellSortVisualizer as Visualizer
 # from vis import Dummy as Visualizer
 from time import time
 
@@ -42,35 +42,35 @@ def main_level_1():
   print('after :', array)
 
 def main():
-  # print('before:', array)
+  print('before:', array)
   count = len(array)
   gap = next_gap(count / 2.5)
-  # print(f'{count=} first_gap={gap}')
+  print(f'{count=} first_gap={gap}')
   while True:
-    # vis.set_gap(gap)
+    vis.set_gap(gap)
     # for offset in range(gap):
     #   start = offset + gap
     #   while start < count:
     for start in range(gap, count):
-        # vis.mark_end(start, True)
+        vis.mark_end(start, True)
         v = array[start]
         i = start
         while i >= gap:
-          # vis.compare(i-gap, i)
+          vis.compare(i-gap, i)
           if array[i - gap] > v:
-            # vis.shift(i-gap, i)
+            vis.shift(i-gap, i)
             array[i] = array[i - gap]
-            # vis.draw()
+            vis.draw()
             i -= gap
           else:
             break
-        # vis.shift(start, i, True)
+        vis.shift(start, i, True)
         array[i] = v
-        # vis.draw()
+        vis.draw()
         start += gap
     gap = next_gap(gap)
     if gap < 1: break
-  # print('after :', array)
+  print('after :', array)
 
 ''' 성능측정
 GAPS = [1750, 701, 301, 141, 63, 31, 15, 7, 3, 1, 0]
@@ -113,20 +113,20 @@ count=500000 elapsed=4.013
 if __name__ == '__main__':
   seed('Hello') # 'Hello' 를 seed 로 고정하여 randint 가 항상 같은 결과가 나오게 한다
 
-  counts = [ 
-    100, 1000, 2000, 3000, 4000, 5000, 
-    6000, 7000, 8000, 9000, 10000, 15000, 
-    20000, 30000, 40000, 50000,
-    100000, 200000, 300000, 400000, 500000,
-  ]
-  for count in counts:
-    array = numbers[:count]
-    shuffle(array)
-    startedOn = time()
-    main()
-    elapsed = time() - startedOn
-    print(f'{count=} {elapsed=:.3f}')
-  exit() 
+  # counts = [ 
+  #   100, 1000, 2000, 3000, 4000, 5000, 
+  #   6000, 7000, 8000, 9000, 10000, 15000, 
+  #   20000, 30000, 40000, 50000,
+  #   100000, 200000, 300000, 400000, 500000,
+  # ]
+  # for count in counts:
+  #   array = numbers[:count]
+  #   shuffle(array)
+  #   startedOn = time()
+  #   main()
+  #   elapsed = time() - startedOn
+  #   print(f'{count=} {elapsed=:.3f}')
+  # exit() 
 
   vis = Visualizer('Selection Sort')
 
