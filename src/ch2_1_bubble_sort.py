116808ebe906b779bd351e8d1055ed28b43457f4
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-09-06 21:42:29 +0900

    Visualizer 사용 준비

--- src/ch2_1_bubble_sort.py
+++ src/ch2_1_bubble_sort.py
@@ -1,13 +1,21 @@
 from data_unsorted import numbers
 # from data_unsorted_a_lot import numbers
 from random import randint, seed
+from vis import BubbleSortVisualizer as Visualizer
+
 
 def main():
   print(array)
 
 if __name__ == '__main__':
   seed('Hello') # 'Hello' 를 seed 로 고정하여 randint 가 항상 같은 결과가 나오게 한다
-  count = randint(10, 30)
-  array = numbers[:count]
-  main()
+  vis = Visualizer('Bubble Sort')
+  while True:
+    count = randint(10, 30)
+    array = numbers[:count]
+    vis.setup(vis.get_main_module())
+    main()
+    vis.draw()
 
+    again = vis.end()
+    if not again: break
