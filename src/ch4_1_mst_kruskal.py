639b5dbe9826e20c9ba705dd7f938e84a9323f53
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-10-30 21:35:53 +0900

    5개까지는 문제가 없다.

--- src/ch4_1_mst_kruskal.py
+++ src/ch4_1_mst_kruskal.py
@@ -55,7 +55,7 @@ def main():
     mst.append((u, v))
     vis.append(u, v, w)
     
-    if (len(mst) == 1): break
+    if (len(mst) == 5): break
 
 if __name__ == '__main__':
   vis = Visualizer('Minimum Spanning Tree - Ksuskal')
