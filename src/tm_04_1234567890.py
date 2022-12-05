76582fa007cd16ccbd9453b8e84f856f451804f5
Kim, Ki-Yong <scgyong@tukorea.ac.kr>
2022-12-05 11:17:43 +0900

    tsp_mst 과제 04반 추가분.

--- src/tm_04_1234567890.py
+++ src/tm_04_1234567890.py
@@ -12,8 +12,6 @@ edges = [
   (12, 17, 494), (13, 14, 383), (13, 17, 479), (14, 16, 694), (15, 16, 392)
 ]
 
-
-
 # 0. union-find 구현
 roots = [ x for x in range(n_vertices) ]
 def union(u, v):
@@ -29,7 +27,15 @@ def find_root(u):
   return roots[u]
 
 # 1. Kruskal Algorithm 으로 MST 구하기
+sortedEdges = sorted(edges, ...)
 mst = []
+while len(mst) < n_vertices - 1:
+  u,v,w = edge = sortedEdges.pop(0)
+  if ...: continue
+  ...
+  mst.append(edge)
+  # mst.append([u,v,w])
+
 print(mst)
 # [[9, 12, 62], [2, 10, 83], [2, 17, 96], [1, 11, 100], [12, 13, 140],
 #  [1, 8, 180], [6, 16, 190], [0, 13, 193], [6, 15, 208], [4, 15, 214], 
@@ -38,22 +44,67 @@ print(mst)
 
 
 # 2. Sequence 구하기
-# 2-1. MST to Adj-Matrix
-adjs = [  ]
+# 2-1. MST to Adj-List
+adjs = [ [] for _ in range(n_vertices) ]
+for u,v,w in mst:
+  adjs[u].append(v)
+  adjs[v].append(u)
 print(adjs)
-# [[13, 5], [11, 8], [10, 17], [12], [15, 14], 
+# [[13, 5], [11, 8], [10, 17], [12], [], 
 #  [0], [16, 15], [15], [1], [12], 
 #  [2, 13, 11], [1, 10, 14], [9, 13, 3], [12, 0, 10], [4, 11], 
-#  [6, 4, 7], [6], [2]]
+#  [4, 7], [], [2]]
 
+# seq = [4,15,6,16,6,...,4]
 
 # 2-2. Adj-Matrix to sequence
+current = start_index = 4
 seq = [ start_index ]
+while adjs[current]: # 더이상 갈 점이 없으면 그만하자
+  # while len(adjs[current]) > 0: # 윗줄과 동일한 의미이다
+
+  # 주위에 있는 점들 중 한번도 안 간 곳이 있으면 그리도 간다
+  # 갔던 점만 있으면 그 중 하나(첫번째) 로 간다 (사실 하나밖에 안남은 상황이다)
+
+  if False:
+    pass
+    # for v in adjs[current]:
+    #   if not v in seq:
+    #     adjs[current].remove(v)
+    #     current = v
+    #     break
+    # else:
+    #   current = adjs[current].pop()
+
+    # for i in range(len(adjs[current])):
+    #   at = i
+    #   if not ... in seq:
+    #     break
+    # current = adjs[current].pop(at)
+
+  for i in range(len(adjs[current])):
+    if not ... in seq:
+      current = adjs[current].pop(i)
+      break
+  else:
+    current = adjs[current].pop()
+
+  seq.append(current) # seq 에 current 를 추가한다
+
+
 print(seq)
 # [4, 15, 6, 16, 6, 15, 7, 15, 4, 14, 11, 1, 8, 1, 11, 10, 2, 17,
 #  2, 10, 13, 12, 9, 12, 3, 12, 13, 0, 5, 0, 13, 10, 11, 14, 4]
 
 # 3. 중복 제거하기
+index = 0
+visited = set()
+while index < len(seq):
+  if seq[index] in visited:
+    seq.pop(index)
+  else:
+    visited.add(seq[index])
+    index += 1
 print(seq)
 # [4, 15, 6, 16, 7, 14, 11, 1, 8, 10, 2, 17, 13, 12, 9, 3, 0, 5, 4]
 
